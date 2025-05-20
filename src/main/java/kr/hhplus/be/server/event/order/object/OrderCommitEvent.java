905c241219ffc4c6beae7cb52093cf66523b8062
leehyokyun <gyrbs22@gmail.com>
2025-05-21 00:05:46 +0900

    Order Event Publisher / Event Listener

---
+++ src/main/java/kr/hhplus/be/server/event/order/object/OrderCommitEvent.java
@@ -0,0 +1,33 @@
+package kr.hhplus.be.server.event.order.object;
+
+/*
+ * 주문까지 COMMIT 완료 시
+ * 이벤트를 전달하기 위한 POJO 객체
+ * */
+public class OrderCommitEvent {
+	//order Id
+	private final Long orderId;
+	
+	//user Id
+	private final Long orderQuantity;
+	
+	/*
+	 * 생성자 주입
+	 * */
+	public OrderCommitEvent(Long orderId, Long orderQuantity) {
+		this.orderId = orderId;
+		this.orderQuantity = orderQuantity;
+	}
+	
+	/*
+	 * Event 객체에서 
+	 * 주문정보와 사용자정보를 전달받기 위함
+	 * */
+	public Long getOrderId() {
+		return this.orderId;
+	}
+	
+	public Long getOrderQuantity() {
+		return this.orderQuantity;
+	}
+}
